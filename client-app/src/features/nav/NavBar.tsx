82a90a7fa1145e951468ad89c0fdc68649d53fd4
Saniya <saniya.nafees@gatech.edu>
2020-04-09 18:41:24 -0400

    Added react router for routing

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,22 +1,22 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { ReactComponent as Logo } from '../../app/layout/assets/dove.svg';
-import ActivityStore from '../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
-  const activityStore = useContext(ActivityStore);
   return (
     <Menu fixed='top' inverted>
       <Container>
-        <Menu.Item header>
+        <Menu.Item header as={NavLink} exact to='/'>
           <Logo style={{ marginRight: '10px' }} />
           Dover
         </Menu.Item>
-        <Menu.Item name='Activities' />
+        <Menu.Item name='Activities' as={NavLink} to='/activities' />
         <Menu.Item>
           <Button
-            onClick={activityStore.openCreateForm}
+            as={NavLink}
+            to='/createActivity'
             positive
             content='Create Activity'
           />
