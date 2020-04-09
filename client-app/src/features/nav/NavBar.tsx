ceaa9695fffe8f1979e004c165029bc0dc569307
Saniya <saniya.nafees@gatech.edu>
2020-04-09 11:13:41 -0400

    Added MobX

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { ReactComponent as Logo } from '../../app/layout/assets/dove.svg';
+import ActivityStore from '../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
-interface IProps {
-  openCreateForm: () => void;
-}
-
-const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
+const NavBar: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
   return (
     <Menu fixed='top' inverted>
       <Container>
@@ -16,13 +15,15 @@ const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
         </Menu.Item>
         <Menu.Item name='Activities' />
         <Menu.Item>
-          <Button onClick={openCreateForm} color='blue'>
-            Create Activity
-          </Button>
+          <Button
+            onClick={activityStore.openCreateForm}
+            positive
+            content='Create Activity'
+          />
         </Menu.Item>
       </Container>
     </Menu>
   );
 };
 
-export default NavBar;
+export default observer(NavBar);
