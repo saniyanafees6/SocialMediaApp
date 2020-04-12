d2534a0c8798ebbeb81a5f792d493f79f907065a
Saniya <saniya.nafees@gatech.edu>
2020-04-11 22:13:29 -0400

    Added styling to fix some components

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Segment, Header, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../app/layout/assets/dove.svg';
 
 const HomePage = () => {
   return (
-    <Container style={{ marginTop: '7em' }}>
-      <h1>Home page</h1>
-      <h3>
-        Go to <Link to='/activities'>Activities</Link>
-      </h3>
-    </Container>
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container text>
+        <Header as='h1' inverted>
+          <Logo style={{ marginRight: '10px' }} />
+          Dover
+        </Header>
+        <Header as='h2' inverted content='Connect with friends and the world around you on Dover.' />
+        <Button as={Link} to='/activities' size='huge' inverted>
+          Explore
+        </Button>
+      </Container>
+    </Segment>
   );
 };
 
