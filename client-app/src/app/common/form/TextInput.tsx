5e48c8d5c2602deb47ace2e4d3cba3e6d5e49cc4
Saniya <saniya.nafees@gatech.edu>
2020-04-13 21:57:25 -0400

    Added reusable react components and client side validation

---
+++ client-app/src/app/common/form/TextInput.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FieldRenderProps } from 'react-final-form';
+import { FormFieldProps, Form, Label } from 'semantic-ui-react';
+
+interface IProps
+  extends FieldRenderProps<string, HTMLInputElement>,
+    FormFieldProps {}
+
+const TextInput: React.FC<IProps> = ({
+  input,
+  width,
+  type,
+  placeholder,
+  meta: { touched, error },
+}) => {
+  return (
+    <Form.Field error={touched && !!error} type={type} width={width}>
+      <input {...input} placeholder={placeholder} type='text' />
+      {touched && error && <Label basic color='red'></Label>}
+    </Form.Field>
+  );
+};
+
+export default TextInput;
